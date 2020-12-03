5588afbe932f79923e3d71b7c02630e9c946968e
imkhw <khw.dev@gmail.com>
2020-12-03 15:49:02 +0630

    Learned props

---
+++ src/books.js
@@ -0,0 +1,15 @@
+export const books = [
+    {
+        id: 1,
+        img: 'https://images-na.ssl-images-amazon.com/images/I/81i8s6M7KRL.__BG0,0,0,0_FMpng_AC_UL210_SR210,210_.jpg',
+        title: "Caste (Oprah's Book Club): The Origins of Our Discontents",
+        author: "Isabel Wilkerson"
+    },
+    {
+        id: 2,
+        img: 'https://images-na.ssl-images-amazon.com/images/I/810Ukmf7bSL._AC_UL210_SR210,210_.jpg',
+        title: "The Coast-to-Coast Murders",
+        author: "James Patterson"
+    }
+]
+
--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+import {books} from './books'
+
+import Book from './Book'
+
 import './index.css'
 
 const BookList = () => {
     return (
         <section className='BookList'>
-            <Book />
-            <Book />
+            {
+                books.map((book) => {
+                    return (
+                        <Book key={book.id} {...book} />
+                    )
+                })
+            }
         </section>
-    )
-}
-
-const Book = () => {
-    return (
-        <article className='Book'>
-            <img src='https://images-na.ssl-images-amazon.com/images/I/81MVfud0o8L._AC_UL210_SR210,210_.jpg' alt='' />
-            <h1>Caste (Oprah's Book Club): The Origins of Our Discontents</h1>
-            <h4>Isabel Wilkerson</h4>
-        </article>
     )
 }
 
