f28c66b34273d6abb315f4cca2bfb73bd7324842
imkhw <khw.dev@gmail.com>
2020-12-03 13:30:11 +0630

    Learned mini book project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const Greeting = () => {
+import './index.css'
+
+const BookList = () => {
     return (
-        <div>
-            Hello world!
-        </div>
-    )   
+        <section className='BookList'>
+            <Book />
+            <Book />
+        </section>
+    )
 }
 
-ReactDom.render(<Greeting />, document.getElementById('root'))
+const Book = () => {
+    return (
+        <article className='Book'>
+            <img src='https://images-na.ssl-images-amazon.com/images/I/81MVfud0o8L._AC_UL210_SR210,210_.jpg' alt='' />
+            <h1>Caste (Oprah's Book Club): The Origins of Our Discontents</h1>
+            <h4>Isabel Wilkerson</h4>
+        </article>
+    )
+}
+
+ReactDom.render(<BookList />, document.getElementById('root'))
