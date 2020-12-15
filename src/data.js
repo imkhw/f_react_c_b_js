6450b7884093d1d5d255872a10785c313d26dda0
imkhw <khw.dev@gmail.com>
2020-12-15 20:46:23 +0630

    Learned useReducer

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const data = [
+    {
+        id: 1, 
+        task: 'Learn react'
+    },
+    {
+        id: 2,
+        task: 'Learn node api'
+    },
+    {
+        id: 3,
+        task: 'Do exercise'
+    },
+    {
+        id: 4,
+        task: 'Take a rest'
+    },
+]
