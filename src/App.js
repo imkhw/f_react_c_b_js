be88a02a9ded9b986840e268780ba43baba8a3c2
imkhw <khw.dev@gmail.com>
2020-12-16 11:48:12 +0630

    Learned prop drilling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import UseReducer from './useReducer'
+import PropDrilling from './PropDrilling'
 
 const App = () => {
     return (
         <React.Fragment>
-            <UseReducer />
+            <PropDrilling />
             <hr />
         </React.Fragment>
     )
